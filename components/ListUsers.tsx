0986e4185b1415ddd5d2829180f3695794d9f510
Alex MacCaw <alex@alexmaccaw.com>
2022-12-19 15:56:48 +1300

    fix async

--- components/ListUsers.tsx
+++ components/ListUsers.tsx
@@ -1,9 +1,11 @@
 import {db, init} from '../app/models/db'
 import {getOldestUser} from '../app/models/user/queries'
-import {useAsyncTupleDatabase} from 'tuple-database/useAsyncTupleDatabase'
+import {useAsyncTupleDatabase} from '../helpers/use-async-tuple-database'
 
 export const ListUsers = () => {
   const oldestUser = useAsyncTupleDatabase(db, getOldestUser, [])
+
+  console.log('oldestUser', oldestUser)
 
   return <div>The oldest user is age: {oldestUser?.age}</div>
 }
