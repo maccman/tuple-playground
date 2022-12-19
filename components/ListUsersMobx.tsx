9a52336622c50f97b651d81a9ce7509800363f07
Alex MacCaw <alex@alexmaccaw.com>
2022-12-19 16:20:33 +1300

    refacto

--- components/ListUsersMobx.tsx
+++ components/ListUsersMobx.tsx
@@ -1,9 +1,8 @@
 import {observer} from 'mobx-react-lite'
 import {useMemo} from 'react'
-import {db, init} from '../app/models/db'
+import {db} from '../app/models/db'
 import {getOldestUser} from '../app/models/user/queries'
 import {MobxSubscribeQueryAsync} from '../helpers/mobx-subscribe-query-async'
-import {useAsyncTupleDatabase} from '../helpers/use-async-tuple-database'
 
 export const ListUsers = observer(() => {
   const oldestUser = useMemo(() => new MobxSubscribeQueryAsync(db, getOldestUser, []), [])
